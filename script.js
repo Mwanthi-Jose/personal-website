2ce1bd0efc928ce4056b802e704c4140a6761725
JOSEPH MWANTHI <101453866+Mwanthi-Jose@users.noreply.github.com>
2024-09-13 13:08:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/*------------------------------------typing animation---------------------------------------*/
+var typed = new Typed(".typing",{
+    strings:[" ", "Web Designer","Web Developer","Graphic Designer", "YouTuber","Animator"],
+    typespeed:50,
+    BackSpeed:05,
+    loop:true
+})
